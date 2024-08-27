718476e56f08be1c2c4cb872a388f103c90e6c09
Rossi Meacham <rossimeacham@gmail.com>
2024-08-27 14:41:11 -0400

    minor adjustment to data

--- src/data/rules/generalcontractors.ts
+++ src/data/rules/generalcontractors.ts
@@ -52,22 +52,5 @@ export const generalContractorRules = [
       url: "https://github.com/pontusab",
       avatar: "https://avatars.githubusercontent.com/u/655158?v=4",
     },
-  },
-  {
-    tags: ["General Contractors"],
-    title: "Dan Clemetson",
-    libs: [],
-    slug: "dan-clemetson",
-    content: `
-    Website: https://danclemetson.com
-    Email: danclemetson@gmail.com
-    Phone: 207-555-1234
-    Area serviced: Midcoast
-    `,
-    author: {
-      name: "Rossi Meacham",
-      url: "https://github.com/CorradoRossi",
-      avatar: "https://avatars.githubusercontent.com/u/3724143?v=4",
-    },
   }
 ];
