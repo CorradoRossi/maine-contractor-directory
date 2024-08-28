d39b7514855c190c934856d79d15cd86728ad787
Rossi Meacham <rossimeacham@gmail.com>
2024-08-28 10:41:29 -0400

    update data

--- src/data/rules/generalcontractors.ts
+++ src/data/rules/generalcontractors.ts
@@ -1,27 +1,16 @@
 export const generalContractorRules = [
   {
     tags: ["General Contractors"],
-    title: "Dan Clemetson",
+    title: "Cold Mountain Builders Inc",
     libs: [],
-    slug: "dan-clemetson",
-    name: `Dan Clemetson Contracting`,
-    email: `danclemetson@gmail.com`,
-    phone: `207-555-1234`,
-    website: `https://danclemetson.com`,
-    area: `Belfast, Midcoast`,
-    about: `
-    What is Lorem Ipsum?
-
-    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-
-Why do we use it?
-It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
-
-
-Where does it come from?
-Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
-
-The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.`,
+    slug: "cold-mountain-builders",
+    name: `Cold Mountain Builders Inc`,
+    email: `coldmtn@coldmtn.com`,
+    phone: `(207) 338-4552`,
+    website: `https://coldmtn.com`,
+    address: `33 Pendleton Aly, Belfast, ME 04915`,
+    area: ``,
+    about: ``,
     content: ``,
     author: {
       name: "Rossi Meacham",
@@ -31,40 +20,222 @@ The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for t
   },
   {
     tags: ["General Contractors"],
-    title: "Charly Rogers",
+    title: "Frey's Carpentry",
     libs: [],
-    slug: "charly-rogers",
-    email: "charlyrogers@gmail.com",
+    slug: "",
+    name: ``,
+    email: ``,
+    phone: ``,
+    website: ``,
+    address: ``,
+    area: ``,
+    about: ``,
     content: `
-    Name: Charly Rogers
-    Email: charlyrogers@gmail.com
-    Phone: 207-555-2345
-    Website: https://charlyrogers.com
-    Area: Belfast, Midcoast
     `,
     author: {
-      name: "Yan Code",
-      url: "https://github.com/yanCode",
-      avatar: "https://avatars.githubusercontent.com/u/2328124?v=4",
+      name: "Rossi Meacham",
+      url: "https://github.com/CorradoRossi",
+      avatar: "https://avatars.githubusercontent.com/u/3724143?v=4",
     },
   },
   {
     tags: ["General Contractors"],
-    title: "Bilbo Baggins",
+    title: "Curtis Ronald Carpentry",
     libs: [],
-    slug: "bilbo-baggins",
-    email: "bilbo@gmail.com",
+    slug: "",
+    name: ``,
+    email: ``,
+    phone: ``,
+    website: ``,
+    address: ``,
+    area: ``,
+    about: ``,
     content: `
-    Name: Bilbo's General Contracting
-    Website: https://bilbobaggins.com
-    Email: bilbo@gmail.com
-    Phone: 207-555-3456
-    Area: Midcoast
     `,
     author: {
-      name: "Pontius Pilate",
-      url: "https://github.com/pontusab",
-      avatar: "https://avatars.githubusercontent.com/u/655158?v=4",
+      name: "Rossi Meacham",
+      url: "https://github.com/CorradoRossi",
+      avatar: "https://avatars.githubusercontent.com/u/3724143?v=4",
+    },
+  },
+  {
+    tags: ["General Contractors"],
+    title: "Duck Trap Woodworking",
+    libs: [],
+    slug: "",
+    name: ``,
+    email: ``,
+    phone: ``,
+    website: ``,
+    address: ``,
+    area: ``,
+    about: ``,
+    content: `
+    `,
+    author: {
+      name: "Rossi Meacham",
+      url: "https://github.com/CorradoRossi",
+      avatar: "https://avatars.githubusercontent.com/u/3724143?v=4",
+    },
+  },
+  {
+    tags: ["General Contractors"],
+    title: "Belfast Carpentry",
+    libs: [],
+    slug: "",
+    name: ``,
+    email: ``,
+    phone: ``,
+    website: ``,
+    address: ``,
+    area: ``,
+    about: ``,
+    content: `
+    `,
+    author: {
+      name: "Rossi Meacham",
+      url: "https://github.com/CorradoRossi",
+      avatar: "https://avatars.githubusercontent.com/u/3724143?v=4",
+    },
+  },
+  {
+    tags: ["General Contractors"],
+    title: "Maine Builders And More",
+    libs: [],
+    slug: "",
+    name: ``,
+    email: ``,
+    phone: ``,
+    website: ``,
+    address: ``,
+    area: ``,
+    about: ``,
+    content: `
+    `,
+    author: {
+      name: "Rossi Meacham",
+      url: "https://github.com/CorradoRossi",
+      avatar: "https://avatars.githubusercontent.com/u/3724143?v=4",
+    },
+  },
+  {
+    tags: ["General Contractors"],
+    title: "The Guy That Fixes Everything",
+    libs: [],
+    slug: "",
+    name: ``,
+    email: ``,
+    phone: ``,
+    website: ``,
+    address: ``,
+    area: ``,
+    about: ``,
+    content: `
+    `,
+    author: {
+      name: "Rossi Meacham",
+      url: "https://github.com/CorradoRossi",
+      avatar: "https://avatars.githubusercontent.com/u/3724143?v=4",
+    },
+  },
+  {
+    tags: ["General Contractors"],
+    title: "Pine Ridge Carpentry",
+    libs: [],
+    slug: "",
+    name: ``,
+    email: ``,
+    phone: ``,
+    website: ``,
+    address: ``,
+    area: ``,
+    about: ``,
+    content: `
+    `,
+    author: {
+      name: "Rossi Meacham",
+      url: "https://github.com/CorradoRossi",
+      avatar: "https://avatars.githubusercontent.com/u/3724143?v=4",
+    },
+  },
+  {
+    tags: ["General Contractors"],
+    title: "Clarke Builders",
+    libs: [],
+    slug: "",
+    name: ``,
+    email: ``,
+    phone: ``,
+    website: ``,
+    address: ``,
+    area: ``,
+    about: ``,
+    content: `
+    `,
+    author: {
+      name: "Rossi Meacham",
+      url: "https://github.com/CorradoRossi",
+      avatar: "https://avatars.githubusercontent.com/u/3724143?v=4",
+    },
+  },
+  {
+    tags: ["General Contractors"],
+    title: "Whitecap Builders",
+    libs: [],
+    slug: "",
+    name: ``,
+    email: ``,
+    phone: ``,
+    website: ``,
+    address: ``,
+    area: ``,
+    about: ``,
+    content: `
+    `,
+    author: {
+      name: "Rossi Meacham",
+      url: "https://github.com/CorradoRossi",
+      avatar: "https://avatars.githubusercontent.com/u/3724143?v=4",
+    },
+  },
+  {
+    tags: ["General Contractors"],
+    title: "R.H. Lambeth Builders",
+    libs: [],
+    slug: "",
+    name: ``,
+    email: ``,
+    phone: ``,
+    website: ``,
+    address: ``,
+    area: ``,
+    about: ``,
+    content: `
+    `,
+    author: {
+      name: "Rossi Meacham",
+      url: "https://github.com/CorradoRossi",
+      avatar: "https://avatars.githubusercontent.com/u/3724143?v=4",
+    },
+  },
+  {
+    tags: ["General Contractors"],
+    title: "",
+    libs: [],
+    slug: "",
+    name: ``,
+    email: ``,
+    phone: ``,
+    website: ``,
+    address: ``,
+    area: ``,
+    about: ``,
+    content: `
+    `,
+    author: {
+      name: "Rossi Meacham",
+      url: "https://github.com/CorradoRossi",
+      avatar: "https://avatars.githubusercontent.com/u/3724143?v=4",
     },
   }
 ];
